2c4cf9f754e66a63b3fb1b6e7d8e218ca9b59bd8
Osman Kadim <Osman.Kadim@adesso.com.tr>
2023-07-22 11:57:11 +0300

    BottomView added.

--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -11,3 +11,4 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val vibrant_blue = Color(0xFF003DFF)
+val bottom_view_color = Color(0xffabb4bd)
--- app/src/main/java/com/example/myapplication/ui/theme/screen/LoginScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/screen/LoginScreen.kt
@@ -1,42 +1,26 @@
 package com.example.myapplication.ui.theme.screen
 
-import android.annotation.SuppressLint
-import android.graphics.drawable.ShapeDrawable
-import androidx.compose.foundation.BorderStroke
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.AbsoluteCutCornerShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -45,39 +29,31 @@ import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shadow
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myapplication.R
+import com.example.myapplication.ui.theme.bottom_view_color
 import com.example.myapplication.ui.theme.vibrant_blue
 
 @Composable
 fun LoginScreen(){
     LoginScreenBackground()
     LoginScreenLogo()
-
-
-    EditText()
-    //Spacer(modifier = Modifier.padding(5.dp))
+    EmailText()
     PasswordText()
     ForgotPasswordText()
     LoginButton()
-
+    BottomView()
 }
 
 @Composable
@@ -108,34 +84,23 @@ fun LoginScreenBackground(){
     }
 }
 
-
-
 @Composable
-private fun EditText(){
-
+private fun EmailText(){
     var email by remember {
         mutableStateOf("")
     }
-
     Column(modifier = Modifier
         .fillMaxSize()
         .padding(16.dp)
         .offset(x = 32.dp, y = 342.dp)
-
-
     ){
-
         Text(
-            text = "Email",
+            text = stringResource(id = R.string.email),
             color = Color.White,
             fontSize = 12.sp,
             modifier = Modifier
                 .padding(bottom = 4.dp)
-
-
-
         )
-        
         BasicTextField(
             value = email,
             onValueChange = {email = it},
@@ -143,39 +108,30 @@ private fun EditText(){
                 .width(310.dp)
                 .height(32.dp)
                 .background(Color.Transparent)
-                .bottomBorder(1.dp, Color.LightGray),
+                .bottomBorder(1.dp, bottom_view_color),
             visualTransformation = VisualTransformation.None,
             textStyle = TextStyle(Color.White),
-            
-
         )
     }
 }
 
 @Composable
 private fun PasswordText(){
-
     var password by remember {
         mutableStateOf("")
     }
-
     Column(modifier = Modifier
         .fillMaxSize()
         .padding(16.dp)
         .offset(x = 32.dp, y = 415.dp)
-
-
     ){
-
         Text(
-            text = "Password",
+            text = stringResource(id = R.string.password),
             color = Color.White,
             fontSize = 12.sp,
             modifier = Modifier
                 .padding(bottom = 4.dp)
-
         )
-
         BasicTextField(
             value = password,
             onValueChange = {password = it},
@@ -183,11 +139,10 @@ private fun PasswordText(){
                 .width(310.dp)
                 .height(32.dp)
                 .background(Color.Transparent)
-                .bottomBorder(1.dp, Color.LightGray),
+                .bottomBorder(1.dp, bottom_view_color),
             visualTransformation = PasswordVisualTransformation(),
             textStyle = TextStyle(Color.White),
-            
-            )
+        )
         Image(painterResource(
             id = R.drawable.ic_eye),
             "content_description_ic_eye",
@@ -198,14 +153,12 @@ private fun PasswordText(){
 
 @Composable
 fun ForgotPasswordText(){
-
     Text(
-        text = "Forgot Password?",
+        text = stringResource(id = R.string.forgot_password),
         style = TextStyle(Color.White),
         modifier = Modifier.offset(x = 265.dp, y = 495.dp),
         fontSize = 12.sp
     )
-
 }
 
 @Composable
@@ -216,35 +169,57 @@ fun LoginButton(){
             .padding(46.dp)
             .fillMaxWidth()
             .offset(y = 515.dp),
-
-
         shape = RectangleShape
     ) {
         Text(
-            text = "Login",
+            text = stringResource(id = R.string.login),
             color = vibrant_blue,
             fontSize = 16.sp,
             textAlign = TextAlign.Center,
             style = TextStyle(
                 fontWeight = FontWeight.Bold
             )
-
         )
     }
+}
 
+@Composable
+private fun BottomView(){
+    Row(
+        modifier = Modifier
+            .padding(start = 105.dp)
+            .fillMaxWidth()
+            .offset(y = 630.dp),
+        horizontalArrangement = Arrangement.spacedBy(2.dp),
+    ) {
+        Text(
+            text = stringResource(id = R.string.dont_have_an_account),
+            fontSize = 12.sp,
+            textAlign = TextAlign.Center,
+            style = TextStyle(
+                fontWeight = FontWeight.Normal,
+                color = bottom_view_color
+            )
+        )
+        Text(
+            text = stringResource(id = R.string.register_now),
+            fontSize = 12.sp,
+            textAlign = TextAlign.Center,
+            style = TextStyle(
+                color = Color.White
+            )
+        )
+    }
 }
 
 
-@Composable
-fun Modifier.bottomBorder(strokeWidth: Dp, color: Color) = composed(
+fun Modifier.bottomBorder(strokeWidth: Dp, color: Color) = composed { composed(
     factory = {
         val density = LocalDensity.current
         val strokeWidthPx = density.run { strokeWidth.toPx() }
-
         Modifier.drawBehind {
             val width = size.width
             val height = size.height - strokeWidthPx
-
             drawLine(
                 color = color,
                 start = Offset(x = 0f, y = height),
@@ -254,5 +229,6 @@ fun Modifier.bottomBorder(strokeWidth: Dp, color: Color) = composed(
         }
     }
 )
+}
 
 
